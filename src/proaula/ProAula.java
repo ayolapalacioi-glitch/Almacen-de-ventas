bd69318975d0bb39c58e05eb6ae0696a6634bc54
isaac david ayola palacio <ayolapalacioi@gmail.com>
2025-09-14 20:48:27 -0500

    cambio nombre de variables

--- src/proaula/ProAula.java
+++ src/proaula/ProAula.java
@@ -1,16 +1,11 @@
 
 package proaula;
 
-import vista.login;
-
-
 public class ProAula {
-
+ 
    
     public static void main(String[] args) {
-        /// instancia del login
-        login login = new login();
-        login.setVisible(true);
+     
     }
     
 }
